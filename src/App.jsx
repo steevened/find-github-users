ac76b5c294d662d9fff392d5cd68d44f175a37fc
steven <alvaradosteven315@gmail.com>
2022-12-07 14:00:50 -0500

    adding footer

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import Loader from './components/Loader';
 import { setDropDown } from './store/slices/dropDown.slice';
 import User from './pages/User';
+import Footer from './layout/Footer';
 
 function App() {
   const loader = useSelector((state) => state.IsLoading);
@@ -16,11 +17,11 @@ function App() {
 
   return (
     <BrowserRouter>
-      <div className="h-full min-h-screen ">
+      <div className="h-full min-h-screen relative">
         <Navbar />
         {loader && <Loader />}
         <main
-          className={`transition-all duration-200 ease-in-out h-full min-h-screen ${
+          className={`transition-all duration-200 ease-in-out h-full pb-24 md:pb-16 ${
             dropdown ? 'blur-sm opacity-5' : ''
           }`}
           onClick={() => dispatch(setDropDown(false))}
@@ -30,6 +31,7 @@ function App() {
             <Route path="/user/:login" element={<User />} />
           </Routes>
         </main>
+        <Footer />
       </div>
     </BrowserRouter>
   );
