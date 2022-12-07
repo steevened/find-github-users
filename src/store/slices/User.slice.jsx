5654b86c0c1c14213a41a7ebfacd6e5ac3b9a9ac
steven <alvaradosteven315@gmail.com>
2022-12-07 09:37:03 -0500

    deploying

--- src/store/slices/User.slice.jsx
+++ src/store/slices/User.slice.jsx
@@ -14,8 +14,8 @@ export const UserSlice = createSlice({
   },
 });
 
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+const GITHUB_URL = import.meta.env.VITE_REACT_APP_GITHUB_URL;
+const GITHUB_TOKEN = import.meta.env.VITE_REACT_APP_GITHUB_TOKEN;
 
 export const UserThunk = (login) => (dispatch) => {
   dispatch(setIsLoading(true));
